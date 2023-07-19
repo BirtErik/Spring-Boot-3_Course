80ba571887c0b34282fd03b74a7f93657f141383
BirtErik <erikbj3@gmail.com>
2023-07-19 16:45:36 +0200

    Add global exception handling

--- 04-spring-boot-rest-crud/03-spring-boot-rest-crud-students-exception-handling/src/main/java/com/restcrud/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-students-exception-handling/src/main/java/com/restcrud/demo/rest/StudentRestController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -39,23 +37,5 @@ public class StudentRestController {
         }
 
         return studentList.get(studentId);
-    }
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(Date.from(Instant.now()));
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage()); //edit for custom message
-        error.setTimeStamp(Date.from(Instant.now()));
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
